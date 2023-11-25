6aad19209230167678eef77d8fdcbb544304f323
Zang <bt.hoanggiang@gmail.com>
2023-11-25 10:12:15 +0700

    create module customer response

---
+++ modules/cs_response/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Customer Response',
+    'version': '1.1',
+    'category': 'Customer Service/Customer Response',
+    'sequence': 95,
+    'summary': 'Centralize employee information',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
