9d5c6c7af6a8b4b6a87a86a988a52d2e5c8141c9
Zang <bt.hoanggiang@gmail.com>
2023-11-25 10:11:27 +0700

    create module customer request

---
+++ modules/cs_request/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Customer Request',
+    'version': '1.1',
+    'category': 'Customer Service/Customer Request',
+    'sequence': 95,
+    'summary': 'Centralize employee information',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
