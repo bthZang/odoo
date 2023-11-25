021b048a061c65271b2319e4b2f265273ac95fe2
Zang <bt.hoanggiang@gmail.com>
2023-11-25 10:12:55 +0700

    create module report request

---
+++ modules/rp_request/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Report Request',
+    'version': '1.1',
+    'category': 'Report/Report Request',
+    'sequence': 95,
+    'summary': 'Centralize employee information',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
