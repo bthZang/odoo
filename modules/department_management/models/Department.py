c3484c04b8a0dab87409ff15a2b20286fcc4da35
Hoang Zangg <bt.hoanggiang@gmail.com>
2023-12-22 17:54:10 +0700

    add total employee in department

--- modules/department_management/models/Department.py
+++ modules/department_management/models/Department.py
@@ -7,3 +7,12 @@ class Department(models.Model):
 
     name = fields.Char(string="Tên phòng ban")
     description = fields.Char(string="Mô tả")
+
+    total_employee = fields.Integer(compute='_compute_total_staff', string='Total Employee')
+
+    def _compute_total_staff(self):
+        emp_data = self.env['human_resource.staff']._read_group([('department', 'in', self.ids)], ['department'],
+                                                                ['department'])
+        result = dict((data['department'][0], data['department_count']) for data in emp_data)
+        for department in self:
+            department.total_employee = result.get(department.id, 0)
