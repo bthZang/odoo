a42b01a6c526ac0b208596ed58003e7c6158be52
Hoang Zangg <bt.hoanggiang@gmail.com>
2023-12-28 02:47:00 +0700

    update timekeeping

--- modules/timekeeping/models/Timekeeping.py
+++ modules/timekeeping/models/Timekeeping.py
@@ -1,10 +1,20 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Timekeeping(models.Model):
     _name = 'human_resource.timekeeping'
     _description = 'All timekeeping list'
 
-    checkin_date = fields.Datetime(string="Thời gian vào")
-    checkout_date = fields.Datetime(string="Thời gian ra")
-    staff = fields.Many2one('human_resource.staff', string='Nhân viên')
+    name = fields.Char(compute='_compute_name')
+
+    checkin_date = fields.Datetime(string="Thời gian vào", required=True)
+    checkout_date = fields.Datetime(string="Thời gian ra", required=True)
+    staff = fields.Many2one('human_resource.staff', string='Nhân viên', required=True)
+
+    @api.depends('checkin_date', 'checkout_date', 'staff')
+    def _compute_name(self):
+        for record in self:
+            if record.checkin_date:
+                record.name = f"{record.staff.name} - {record.checkin_date.strftime('%d/%m/%Y')}"
+            else:
+                record.name = "New timekeeping"
